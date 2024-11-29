63e31fa3210a6ddf3bd71a0ce9c726bb128d99db
pedroacucalon <antoniocc-29@hotmail.com>
2024-11-29 08:13:25 -0500

    Inicio de proyecto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
+  emailForm: FormGroup;
+  message: string | null = null;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.emailForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
 
+  get email() {
+    return this.emailForm.get('email')!;
+  }
+
+  onSubmit() {
+    if (this.emailForm.valid) {
+      this.message = `Correo enviado a: ${this.email.value}`;
+    } else {
+      this.message = null;
+    }
+  }
 }
